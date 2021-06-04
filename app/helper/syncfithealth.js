311bfebdb9703f3a7d94bfffd41f6e049824e419
Praveen Murugesan <lefthandmagic@gmail.com>
2021-06-04 15:32:25 +0200

    add sync task

--- app/helper/syncfithealth.js
+++ app/helper/syncfithealth.js
@@ -1,4 +1,9 @@
 import {updateWeight} from "../components/apphealth";
+import * as TaskManager from 'expo-task-manager';
+import * as BackgroundFetch from 'expo-background-fetch';
+
+const BACKGROUND_TASK_NAME = 'background-sync-task';
+
 
 
 export function SyncFitHealth(access_token) {
@@ -21,4 +26,40 @@ export function SyncFitHealth(access_token) {
 
   export function ScheduleSyncFitHealth(accessToken) {
       console.log("schedule sync here")
+      TaskManager.isTaskRegisteredAsync(BACKGROUND_TASK_NAME)
+      .then(res => {
+        console.log(res)
+        if (!res) {
+          const options =  {
+            minimumInterval: 30,
+            stopOnTerminate: false,
+            startOnBoot: true
+          }
+          BackgroundFetch.registerTaskAsync(BACKGROUND_TASK_NAME,
+            options)
+            console.log("registeredTask")
+          }
+      });
   }
+
+  TaskManager.defineTask(BACKGROUND_TASK_NAME, () => {
+    try {
+      const receivedNewData = fetch('https://api.fitbit.com/1.2/user/-/body/log/weight/date/2021-05-30.json', {
+        method: 'GET',
+        headers: {
+         Authorization: `Bearer ${access_token}`,
+        },
+        })
+        .then(res => res.json())
+        .then(res => {
+          weightArray = res.weight;
+          updateWeight(weightArray[0].weight);
+        })
+        console.log("registered task running")
+      return receivedNewData ? BackgroundFetch.Result.NewData : BackgroundFetch.Result.NoData;
+    } catch (error) {
+      return BackgroundFetch.Result.Failed;
+    }
+  });
+  
+  
